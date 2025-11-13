d094c0ee6a5e7671e38b948a240199b9c9155e05
Layo Mendes <165064021+LayoMendes@users.noreply.github.com>
2025-11-13 11:33:49 +0000

    base-criada

---
+++ calculadora/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Calculator from './components/Calculator';
+import './index.css';
+
+function App() {
+  return (
+    <div className="app">
+      
+      <Calculator />
+      <p>Developed by Layo</p>
+    </div>
+  );
+}
+
+export default App;
---
+++ calculadora/src/components/Calculator.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import './Calculator.css';
+
+function Calculator() {
+  return (
+    <div className="calculator">
+      <div className="display">
+        0
+      </div>
+      <div className="buttons">
+        <button>C</button>
+        <button>±</button>
+        <button>%</button>
+        <button>÷</button>
+
+        <button>7</button>
+        <button>8</button>
+        <button>9</button>
+        <button>×</button>
+
+        <button>4</button>
+        <button>5</button>
+        <button>6</button>
+        <button>-</button>
+
+        <button>1</button>
+        <button>2</button>
+        <button>3</button>
+        <button>+</button>
+
+        <button className="zero">0</button>
+        <button>.</button>
+        <button>=</button>
+      </div>
+    </div>
+  );
+}
+
+export default Calculator;
